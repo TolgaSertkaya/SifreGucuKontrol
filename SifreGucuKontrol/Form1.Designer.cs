b6f9d227d91b7c750a305dfa6a56793c1841813b
lenovo <lenovo@LAPTOP-8GNSB8G4>
2022-07-21 16:50:58 +0300

    Add project files.

---
+++ SifreGucuKontrol/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿
+namespace SifreGucuKontrol
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtSifreGirisi = new System.Windows.Forms.TextBox();
+            this.lblsifreGücü = new System.Windows.Forms.Label();
+            this.lblrenk = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtSifreGirisi
+            // 
+            this.txtSifreGirisi.Location = new System.Drawing.Point(150, 99);
+            this.txtSifreGirisi.Name = "txtSifreGirisi";
+            this.txtSifreGirisi.Size = new System.Drawing.Size(166, 20);
+            this.txtSifreGirisi.TabIndex = 0;
+            this.txtSifreGirisi.TextChanged += new System.EventHandler(this.txtSifreGirisi_TextChanged);
+            // 
+            // lblsifreGücü
+            // 
+            this.lblsifreGücü.AutoSize = true;
+            this.lblsifreGücü.Location = new System.Drawing.Point(147, 134);
+            this.lblsifreGücü.Name = "lblsifreGücü";
+            this.lblsifreGücü.Size = new System.Drawing.Size(60, 13);
+            this.lblsifreGücü.TabIndex = 3;
+            this.lblsifreGücü.Text = "Şifre Gücü:";
+            // 
+            // lblrenk
+            // 
+            this.lblrenk.AutoSize = true;
+            this.lblrenk.Location = new System.Drawing.Point(278, 134);
+            this.lblrenk.Name = "lblrenk";
+            this.lblrenk.Size = new System.Drawing.Size(38, 13);
+            this.lblrenk.TabIndex = 4;
+            this.lblrenk.Text = "Düşük";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(478, 249);
+            this.Controls.Add(this.lblrenk);
+            this.Controls.Add(this.lblsifreGücü);
+            this.Controls.Add(this.txtSifreGirisi);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtSifreGirisi;
+        private System.Windows.Forms.Label lblsifreGücü;
+        private System.Windows.Forms.Label lblrenk;
+    }
+}
+
---
+++ SifreGucuKontrol/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SifreGucuKontrol
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            //şifre girişi alın.
+            //şifre 6 karakterden az ise şifre gücü düşük yazsın.Rengi kırmızı olsun.
+            //şifre 6 karakterden çok ama tek tip karakter ise.Rengi kırmızı olsun.
+            //şifre 6 karakterden fazla ve sayı+özelkarakter+harf içeriyorsa güçlü (rengi yeşil olsun)
+            //şifre 6 karakterden fazla ve ikili kombinasyon içeriyorsa (harf+sayı/harf+özel karakter) orta desin rengi mavi olsun
+
+        }
+
+
+        private void txtSifreGirisi_TextChanged(object sender, EventArgs e)
+        {
+
+            string sifre = txtSifreGirisi.Text;
+            if (sifre.Length < 6 ||sifre.All(char.IsLetter)|| sifre.All(char.IsDigit)||!sifre.Any(char.IsLetterOrDigit))
+            {
+                lblrenk.Text = "Düşük";
+                lblrenk.ForeColor = Color.Red;
+            }
+            else if (sifre.Any(char.IsLetter)&& sifre.Any(char.IsDigit) && !sifre.All(char.IsLetterOrDigit))
+            {
+                lblrenk.Text = "Yüksek";
+                lblrenk.ForeColor = Color.Green;
+            }
+            else
+            {
+                lblrenk.Text = "Orta";
+                lblrenk.ForeColor = Color.Blue;
+            }
+
+        }
+    }
+}
